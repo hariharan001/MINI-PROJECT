c1b8f4c53c7c6a020d37832d1c30b89c35aaa043
user2 <user2@simlab26>
2019-10-16 23:02:09 +0530

    developed by hariharan

--- mini project/src/gradecalculator/AnnaUniv.java
+++ mini project/src/gradecalculator/AnnaUniv.java
@@ -12,6 +12,7 @@ public class AnnaUniv
 				System.out.println("1. TO CALCULATE YOUR GPA FOR 3 SUBJECTS");
 				System.out.println("2. TO CALCULATE YOUR GPA FOR 8 SUBJECTS");
 				System.out.println("3. TO CALCULATE YOUR GPA FOR 9 SUBJECTS");
+				System.out.println("4. TO CALCULATE YOUR CGPA ");
 				System.out.print("Enter your choice:");
 				Scanner r = new Scanner(System.in);
 				int option=r.nextInt();
@@ -22,171 +23,181 @@ public class AnnaUniv
 				case 1:
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 1: ");
-					int  x1=r.nextInt();
-				
-						
-					System.out.print("ENTER THE CREDITS OF SUBJECT 1: ");
-					int x2=r.nextInt();
-					int x3=x1*x2;
+					String  x1=r.next();
+				    System.out.print("ENTER THE CREDITS OF SUBJECT 1: ");
+					float x2=r.nextFloat();
+					float a11=Conversion.strtoINT(x1,x2);
+					
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 2: ");
-					int  x4=r.nextInt();
+					String x4=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 2: ");
-					int x5=r.nextInt();
-					int x6=x4*x5;
+					float x5=r.nextFloat();
+					float a12=Conversion.strtoINT(x4,x5);
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 3: ");
-					int x7=r.nextInt();
+					String x7=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 3: ");
-					int x8=r.nextInt();
-					int x9=x7*x8;
-					int gpa=(x3+x6+x9)/(x2+x4+x8);
+					float x8=r.nextFloat();
+					float a13=Conversion.strtoINT(x7,x8);
+					float gpa=(a11+a12+a13)/(x2+x5+x8);
 					System.out.println("GPA :"+gpa);
 					break;
 				
 				case 2:
 				
 					System.out.println("ENTER THE GRADE OF SUBJECT 1: ");
-					int  x10=r.nextInt();
+					String  x10=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 1: ");
-					int x11=r.nextInt();
-					int x12=x10*x11;
+					float x11=r.nextFloat();
+					float a14=Conversion.strtoINT(x10,x11);
+					
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 2: ");
-					int  x13=r.nextInt();
+					String  x13=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 2: ");
-					int x14=r.nextInt();
-					int x15=x13*x14;
+					float x14=r.nextFloat();
+					float a15=Conversion.strtoINT(x13,x14);
+					
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 3: ");
-					int x16=r.nextInt();
+					String x16=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 3: ");
-					int x17=r.nextInt();
-					int x18=x16*x17;
+					float x17=r.nextFloat();
+					float a16=Conversion.strtoINT(x16,x17);
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 4: ");
-					int  x19=r.nextInt();
+					String  x19=r.next();
 				    System.out.print("ENTER THE CREDITS OF SUBJECT 4: ");
-					int x20=r.nextInt();
-					int x21=x19*x20;
+				    float x20=r.nextFloat();
+					float a17=Conversion.strtoINT(x19,x20);
+				 
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 5: ");
-					int  x22=r.nextInt();
+					String x22=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 5: ");
-					int x23=r.nextInt();
-					int x24=x22*x23;
+					float x23=r.nextFloat();
+					float a18=Conversion.strtoINT(x22,x23);
+					
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 6: ");
-					int x25=r.nextInt();
+					String x25=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 6: ");
-					int x26=r.nextInt();
-					int x27=x25*x26;
+					float x26=r.nextFloat();
+					float a19=Conversion.strtoINT(x25,x26);
+					
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 7: ");
-					int  x28=r.nextInt();
+					String  x28=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 7: ");
-					int x29=r.nextInt();
-					int x30=x28*x29;
+					float x29=r.nextFloat();
+					float a20=Conversion.strtoINT(x28,x29);
+					
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 8: ");
-					int  x31=r.nextInt();
+					String  x31=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 8: ");
-					int x32=r.nextInt();
-					int x33=x31*x32;
+					float x32=r.nextFloat();
+					float a21=Conversion.strtoINT(x31,x32);
 					
 					
-					double gpa1=(x12+x15+x18+x21+x24+x27+x30+x33)/(x11+x14+x17+x20+x23+x26+x29+x32);
+					
+					double gpa1=(a14+a15+a16+x17+a18+a19+a20+a21)/(x11+x14+x17+x20+x23+x26+x29+x32);
 					System.out.println(" GPA: "+gpa1);
 					break;
 	
 				case 3:
 					
 					System.out.println("ENTER THE GRADE OF SUBJECT 1: ");
-					int  x37=r.nextInt();
+					String  x37=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 1: ");
-					int x38=r.nextInt();
-					int x39=x38*x37;
+					float x38=r.nextFloat();
+					float a22=Conversion.strtoINT(x37,x38);
+					
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 2: ");
-					int  x40=r.nextInt();
+					String  x40=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 2: ");
-					int x41=r.nextInt();
-					int x42=x40*x41;
+					float x41=r.nextFloat();
+					float a23=Conversion.strtoINT(x40,x41);
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 3: ");
-					int x43=r.nextInt();
+					String x43=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 3: ");
-					int x44=r.nextInt();
-					int x45=x43*x44;
+					float x44=r.nextFloat();
+					float a24=Conversion.strtoINT(x43,x44);
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 4: ");
-					int  x46=r.nextInt();
+					String  x46=r.next();
 				    System.out.print("ENTER THE CREDITS OF SUBJECT 4: ");
-					int x47=r.nextInt();
-					int x48=x46*x47;
+				    float x47=r.nextFloat();
+				    float a25=Conversion.strtoINT(x46,x47);
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 5: ");
-					int  x49=r.nextInt();
+					String  x49=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 5: ");
-					int x50=r.nextInt();
-					int x51=x49*x50;
+					float x50=r.nextFloat();
+					float a26=Conversion.strtoINT(x49,x50);
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 6: ");
-					int x52=r.nextInt();
+					String x52=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 6: ");
-					int x53=r.nextInt();
-					int x54=x52*x53;
+					float x53=r.nextFloat();
+					float a27=Conversion.strtoINT(x52,x53);
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 7: ");
-					int  x55=r.nextInt();
+					String  x55=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 7: ");
-					int x56=r.nextInt();
-					int x57=x55*x56;
+					float x56=r.nextFloat();
+					float a28=Conversion.strtoINT(x55,x56);
 					
 				
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 8: ");
-					int  x58=r.nextInt();
+					String  x58=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 8: ");
-					int x59=r.nextInt();
-					int x60=x58*x59;
+					float x59=r.nextFloat();
+					float a29=Conversion.strtoINT(x58,x59);
 					
 					System.out.print("ENTER THE GRADE OF SUBJECT 9: ");
-					int  x61=r.nextInt();
+					String  x61=r.next();
 					System.out.print("ENTER THE CREDITS OF SUBJECT 9: ");
-					int x62=r.nextInt();
-					int x63=x61*x62;
+					float x62=r.nextFloat();
+					float a30=Conversion.strtoINT(x61,x62);
 					
-					System.out.print("ENTER THE GRADE OF SUBJECT 9: ");
-					int  x64=r.nextInt();
-					System.out.print("ENTER THE CREDITS OF SUBJECT 9: ");
-					int x65=r.nextInt();
-					int x66=x64*x65;
-					double gpa2=(x39+x42+x45+x48+x51+x54+x57+x60+x63+x66)/(x38+x41+x44+x47+x50+x53+x56+x59+x62+x65);
+					
+					float gpa2=(a22+a23+a24+a25+a26+a27+a28+a29+a30)/(x38+x41+x44+x47+x50+x53+x56+x59+x62);
 					System.out.println(" GPA: "+gpa2);
 					break;
+				case 4:
+					System.out.println("ENTER THE NUMBER OF COMPLETED SEMESTERS");
+					float g=r.nextFloat();
+					float b1=Conversion.cgpa(g);
+					System.out.println("YOUR CGPA IS:"+b1);
+					break;
+					
 				default:
 					System.out.println("Please enter a valid data!!!");
 				
---
+++ mini project/src/gradecalculator/Conversion.java
@@ -0,0 +1,172 @@
+package gradecalculator;
+import java.lang.*;
+import java.util.Scanner;
+
+public class Conversion 
+	{
+	public static float strtoINT(String x1,float x2)
+	{
+		
+		float j1,x=0;
+		
+		
+		if(x1.equals("b"))
+		{
+			j1=6.0f;	
+			x=j1*x2;
+		}    
+		
+		else if (x1.equals("bplus"))
+		{
+			j1=7.0f;
+		    x=j1*x2;
+		}
+		
+		
+		else if (x1.equals("a"))
+		{
+			j1=8.0f;
+			x=j1*x2;
+		}
+		
+		else if (x1.equals("aplus"))
+		{
+			j1=9.0f;
+			x=j1*x2;
+		}
+		else if (x1.equals("o"))
+		{
+			j1=10.0f;
+			x=j1*x2;
+		}
+		else
+		{
+			System.out.println("Enter a valid grade");
+			return (Float) null;
+		}
+		return x;		
+	}
+	public static float cgpa(float g)
+	{
+		float f1=0;
+		Scanner sc=new Scanner(System.in);
+		if(g==2)
+		{
+			
+			
+			System.out.println("ENTER THE GPA OF SEM 1");
+			 float q1=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 2");
+			float q2=sc.nextFloat();
+			 f1=(q1+q2)/2;
+		}    
+		
+		else if (g==3)
+		{
+
+			System.out.println("ENTER THE GPA OF SEM 1");
+			 float q1=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 2");
+			float q2=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 3");
+			 float q3=sc.nextFloat();
+			 f1=(q1+q2+q3)/3;
+		}
+		
+		else if (g==4)
+		{
+
+			System.out.println("ENTER THE GPA OF SEM 1");
+			 float q1=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 2");
+			float q2=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 3");
+			 float q3=sc.nextFloat();
+			 System.out.println("ENTER THE GPA OF SEM 4");
+			 float q4=sc.nextFloat();
+		 f1=(q1+q2+q3+q4)/4;
+		}
+		
+		
+		else if (g==5)
+		{
+
+			System.out.println("ENTER THE GPA OF SEM 1");
+			float q1=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 2");
+			float q2=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 3");
+			float q3=sc.nextFloat();
+			 System.out.println("ENTER THE GPA OF SEM 4");
+			 float q4=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 5");
+			float q5=sc.nextFloat();
+			 f1=(q1+q2+q3+q4+q5)/5;
+		}
+		
+		else if (g==6)
+		{
+
+			System.out.println("ENTER THE GPA OF SEM 1");
+			float q1=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 2");
+			float q2=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 3");
+			float q3=sc.nextFloat();
+			 System.out.println("ENTER THE GPA OF SEM 4");
+			 float q4=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 5");
+			float q5=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 6");
+			float q6=sc.nextFloat();
+			 f1=(q1+q2+q3+q4+q5+q6)/6;
+		}
+		
+		else if (g==7)
+		{
+
+			System.out.println("ENTER THE GPA OF SEM 1");
+			float q1=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 2");
+			float q2=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 3");
+			float q3=sc.nextFloat();
+			 System.out.println("ENTER THE GPA OF SEM 4");
+			 float q4=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 5");
+			float q5=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 6");
+			float q6=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 7");
+			float q7=sc.nextFloat();
+			
+			 f1=(q1+q2+q3+q4+q5+q6+q7)/7;
+		}
+		
+		else if (g==8)
+		{
+
+			System.out.println("ENTER THE GPA OF SEM 1");
+			float q1=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 2");
+			float q2=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 3");
+			float q3=sc.nextFloat();
+			 System.out.println("ENTER THE GPA OF SEM 4");
+			 float q4=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 5");
+			float q5=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 6");
+			float q6=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 7");
+			float q7=sc.nextFloat();
+			System.out.println("ENTER THE GPA OF SEM 8");
+			float q8=sc.nextFloat();
+			 f1=(q1+q2+q3+q4+q5+q6+q7+q8)/8;
+		}
+		
+		return f1;
+	
+		
+			}
+	}
